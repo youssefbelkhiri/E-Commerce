76d30e3e5ff02f47a339fbdc99147c55f2ea49fa
youssef belkhiri <belkhiriyoussef33@gmail.com>
2024-10-17 19:18:19 +0100

    order service v1

---
+++ Services/order/src/main/java/com/example/ecommerce/orderLine/OrderLine.java
@@ -0,0 +1,23 @@
+package com.example.ecommerce.orderLine;
+
+
+import com.example.ecommerce.order.Order;
+import jakarta.persistence.*;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+public class OrderLine {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    @ManyToOne
+    @JoinColumn(name="order_id")
+    private Order order;
+    private Integer productId;
+    private double quantity;
+}
